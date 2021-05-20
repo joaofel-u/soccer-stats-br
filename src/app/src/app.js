80398d1bdb7fadd560be275a6d16b4ba3acb81cd
João Fellipe <felippe.uller@gmail.com>
2021-05-20 01:17:48 -0300

    [app] Defined Consumer Application

---
+++ src/app/src/app.js
@@ -0,0 +1,30 @@
+import https from 'https'
+import fs from 'fs'
+import path from 'path'
+
+import express from 'express'
+import {consultaInicial, consultaPesquisaPorAltura} from './controle'
+
+const porta = 3000
+const msgNoAr = `Servidor no ar, porta ${porta}`
+const opcoes = {
+  key: fs.readFileSync(path.resolve(__dirname, '../cert/key.pem')),
+  cert: fs.readFileSync(path.resolve(__dirname, '../cert/cert.pem'))
+}
+
+const rawData = fs.readFileSync('src/out.json')
+
+const dados = JSON.parse(rawData)
+
+const app = express()
+
+app.set('view engine', 'pug')
+
+app.get('/', (req, res) => consultaInicial(res, dados))
+
+app.get('/pesquisePorAltura', (req, res) => consultaPesquisaPorAltura(req, res, dados))
+
+const server = https.createServer(opcoes, app)
+
+// eslint-disable-next-line no-console
+server.listen(porta, () => console.log(msgNoAr))
